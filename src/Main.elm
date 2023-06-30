1fdf6fce741d2d944bb5ab0adf4833d5640321a6
Søren Skovsbøll <soren.skovsboll@crowdstrike.com>
2023-06-30 17:52:21 +0200

    crude syntax hl

--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,7 @@ import Html as H
 import Html.Attributes as A
 import Html.Events as E
 import Json.Decode as J
-import Parser as P
+import Parser as P exposing ((|.))
 
 
 type Msg
@@ -21,6 +21,7 @@ type alias ScrollPos =
 
 type alias Model =
     { text : String
+    , ast : List Line
     , scroll : ScrollPos
     }
 
@@ -37,7 +38,8 @@ main =
 
 init : {} -> ( Model, Cmd Msg )
 init _ =
-    ( { text =
+    let
+        defaultSrc =
             """url=/^\\/add_to_cart\\/(?<product_id>\\d+)/
 | match(file="products.csv", column=product_id, field=product_id)
 | sum(product_price, as="Total revenue")
@@ -51,7 +53,10 @@ init _ =
 // Maybe add this
 | timechart(field=#kind)
 | sankey(from=lallerko, to=nollerkok)"""
+    in
+    ( { text = defaultSrc
       , scroll = scrollTop
+      , ast = parseHql defaultSrc
       }
     , Cmd.none
     )
@@ -68,7 +73,7 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Input text ->
-            ( { model | text = text }, Cmd.none )
+            ( { model | text = text, ast = parseHql text }, Cmd.none )
 
         Scroll pos ->
             ( { model | scroll = pos }, Cmd.none )
@@ -96,12 +101,12 @@ view model =
             ]
             [ H.pre [ A.class "p-2 m-0 align-left h-full bg-transparent" ]
                 [ H.code []
-                    (toLinesOfFragments model.text
+                    (model.ast
                         |> List.indexedMap
                             (\idx fragments ->
                                 H.node "code-line"
                                     [ A.class "block pl-[0px]"
-                                    , A.class "before:content-[attr(data-lino)] before:inline-block before:align-right before:w-[40px] before:p-0 before:pr-[20px] before:opacity-25"
+                                    , A.class "before:content-[attr(data-lino)] before:inline-block before:text-right before:w-[40px] before:p-0 before:pr-[20px] before:opacity-25 before:text-white"
                                     , A.attribute "data-lino" (String.fromInt idx)
                                     ]
                                     (fragments
@@ -141,6 +146,55 @@ styles =
     """
 
 
+type alias Token =
+    ( Syntax, String )
+
+
+type Syntax
+    = Other
+    | LineBreak
+    | Comment
+    | Symbol
+    | StringLiteral
+
+
+tokenToFragment : Token -> Fragment
+tokenToFragment ( syntax, text ) =
+    case syntax of
+        Other ->
+            { text = text, color = "text-slate-500", tag = "other" }
+
+        Comment ->
+            { text = text, color = "text-white-200", tag = "comment" }
+
+        Symbol ->
+            { text = text, color = "text-blue-600", tag = "symbol" }
+
+        StringLiteral ->
+            { text = text, color = "text-orange-600", tag = "symbol" }
+
+        LineBreak ->
+            { text = text, color = "", tag = "" }
+
+
+parseHql : String -> List Line
+parseHql src =
+    let
+        defaultLines : List Line
+        defaultLines =
+            src
+                |> String.split "\n"
+                |> List.map (\l -> [ { text = l ++ "\n", tag = "no-syntax", color = "" } ])
+    in
+    P.run toRevTokens src
+        |> Result.map toLines
+        |> Result.withDefault defaultLines
+
+
+type alias Line =
+    List Fragment
+
+
 type alias Fragment =
     { text : String
     , tag : String
@@ -148,8 +202,87 @@ type alias Fragment =
     }
 
 
-toLinesOfFragments : String -> List (List Fragment)
-toLinesOfFragments txt =
-    txt
-        |> String.split "\n"
-        |> List.map (\l -> [ { text = l ++ "\n", tag = "identifier", color = "text-green-400" } ])
+toLines : List Token -> List (List Fragment)
+toLines revTokens =
+    let
+        help : Token -> ( List Line, List Fragment ) -> ( List Line, List Fragment )
+        help (( syntax, _ ) as token) ( lines, fragments ) =
+            case syntax of
+                LineBreak ->
+                    ( fragments :: lines, [] )
+
+                _ ->
+                    ( lines, tokenToFragment token :: fragments )
+    in
+    List.foldl help ( [], [] ) revTokens
+        |> (\( lines, fragments ) -> fragments :: lines)
+
+
+toRevTokens : P.Parser (List Token)
+toRevTokens =
+    P.loop [] mainLoop
+
+
+mainLoop : List Token -> P.Parser (P.Step (List Token) (List Token))
+mainLoop revTokens =
+    P.oneOf
+        [ whitespace
+            |> P.map (\n -> P.Loop (n :: revTokens))
+        , P.chompIf ((==) '"')
+            |> P.getChompedString
+            |> P.map (\b -> P.Loop (( StringLiteral, b ) :: revTokens))
+        , P.chompIf isSymbol
+            |> P.getChompedString
+            |> P.map (\b -> P.Loop (( Symbol, b ) :: revTokens))
+        , P.chompIf (always True)
+            |> P.getChompedString
+            |> P.map (\b -> P.Loop (( Other, b ) :: revTokens))
+        , P.succeed (P.Done revTokens)
+        ]
+
+
+
+-- Helpers
+
+
+isSymbol : Char -> Bool
+isSymbol char =
+    String.contains (String.fromChar char) symbols
+
+
+symbols : String
+symbols =
+    ".,_-!*?+;:<>#%&/|"
+
+
+whitespace : P.Parser Token
+whitespace =
+    P.oneOf
+        [ space
+        , lineBreak
+        ]
+
+
+isSpace : Char -> Bool
+isSpace c =
+    c == ' ' || c == '\t'
+
+
+space : P.Parser Token
+space =
+    chompIfThenWhile isSpace
+        |> P.getChompedString
+        |> P.map (\b -> ( Other, b ))
+
+
+lineBreak : P.Parser Token
+lineBreak =
+    P.symbol "\n"
+        |> P.map (\_ -> ( LineBreak, "\n" ))
+
+
+chompIfThenWhile : (Char -> Bool) -> P.Parser ()
+chompIfThenWhile isNotRelevant =
+    P.succeed ()
+        |. P.chompIf isNotRelevant
+        |. P.chompWhile isNotRelevant
